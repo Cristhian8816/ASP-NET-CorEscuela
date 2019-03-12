77bf81cedfbcc9efbb3dfd99f63b3056e759a1ce
Cristhian Julian Gomez Lizcano <cristhian.julian@hotmail.com>
2019-03-12 17:22:18 -0500

    Vistas Parciales

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Curso_ASP_NET_Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,42 @@ namespace Curso_ASP_NET_Core.Controllers
     {
         public IActionResult Index()
         {
-            var asignatura = new Asignatura();
-
-            asignatura.UniqueId = Guid.NewGuid().ToString();
-            asignatura.Nombre = "Programacion";
+            return View(new Asignatura { 
+                                Nombre = "Programacion",
+                                UniqueId = Guid.NewGuid().ToString()
+                               }
+                        );
+        }
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>()
+            {
+                new Asignatura { 
+                                 Nombre = "Matematicas",
+                                 UniqueId = Guid.NewGuid().ToString()
+                               },
+                new Asignatura {
+                                 Nombre = "Educacion Fisica",
+                                 UniqueId = Guid.NewGuid().ToString()
+                               },
+                new Asignatura { 
+                                 Nombre = "Castellano",
+                                 UniqueId = Guid.NewGuid().ToString()
+                               },
+                new Asignatura { 
+                                 Nombre = "Ciencias Naturales",
+                                 UniqueId = Guid.NewGuid().ToString()
+                               },
+                new Asignatura { 
+                                 Nombre = "Programacion",
+                                 UniqueId = Guid.NewGuid().ToString()
+                               },
+            };        
 
             ViewBag.CosaDinamica = "Cristhian is the best";
             ViewBag.Fecha = DateTime.Now;
-            
-            return View(asignatura);
+
+            return View("MultiAsignatura",listaAsignaturas);
         }
     }
 }
