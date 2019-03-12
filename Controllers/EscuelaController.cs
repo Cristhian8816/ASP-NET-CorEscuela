cba1684193717f282e512ecb7c65d5ba7f131a35
Cristhian Julian Gomez Lizcano <cristhian.julian@hotmail.com>
2019-03-12 12:29:36 -0500

    MVC a nivel Basico

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,20 @@
+using System;
+using Curso_ASP_NET_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso_ASP_NET_Core.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var escuela = new Escuela();
+
+            escuela.añoFundacion = 2005;
+            escuela.EscuelaId = Guid.NewGuid().ToString();
+            escuela.Nombre = "Platzi School";
+            
+            return View(escuela);
+        }
+    }
+}
---
+++ Models/Escuela.cs
@@ -0,0 +1,10 @@
+namespace Curso_ASP_NET_Core.Models
+{
+    public class Escuela
+    {
+        public string EscuelaId {get; set;}
+        public string Nombre {get; set;}
+        public int añoFundacion {get; set;}
+
+    }
+}
